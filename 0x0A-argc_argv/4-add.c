fb5d6366d43ca1a0b10b7de569935a5f25c6f049
DennisMatenge <mateshdennis@gmail.com>
2023-03-10 04:25:24 -0800

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -31,13 +31,13 @@
 	}
 
 
-	/**
-	 * main - Print the name of the program
-	 * @argc: Count arguments
-	 * @argv: Arguments
-	 *
-	 * Return: Always 0 (Success)
-	 */
+/**
+* main - Print the name of the program
+* @argc: Count arguments
+* @argv: Arguments
+*
+* Return: Always 0 (Success)
+*/
 
 
 	int main(int argc, char *argv[])
